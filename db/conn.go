ef02be05bb51d043a6e33d16a7e367fad0960da6
Nuttavut Thongjor <n.thongjor@gmail.com>
2022-11-30 21:23:50 +0700

    rollback

--- db/conn.go
+++ db/conn.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"os"
 
-	"gorm.io/driver/postgres"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
@@ -14,7 +14,7 @@ var Conn *gorm.DB
 
 func ConnectDB() {
 	dsn := os.Getenv("DATABASE_DSN")
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{Logger: logger.Default.LogMode(logger.Info)})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{Logger: logger.Default.LogMode(logger.Info)})
 	if err != nil {
 		log.Fatal("Cannot connect to the database")
 	}
